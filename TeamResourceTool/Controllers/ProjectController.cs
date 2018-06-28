9e73ed0d90b40e6ea05ce829e6835c135afdd851
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2018-06-28 16:00:09 -0700

    Load Project resources + new views

---
+++ TeamResourceTool/Controllers/ProjectController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamResourceTool.Models;
+using TeamResourceTool.ViewModels;
+
+namespace TeamResourceTool.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ProjectController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Project
+        public ActionResult Index()
+        {
+            var projects = _context.Project.ToList();
+            return View(projects);
+        }
+
+        [HttpGet]
+        public ActionResult Details(byte teamID, int projectID)
+        {
+            TempData["ProjectID"] = projectID;
+            TempData["TeamID"] = teamID;
+
+            var viewModel = new ProjectDetails
+            {
+                Project = _context.Project.Where(p => p.Id == projectID).FirstOrDefault(),
+                Resources = _context.Project.Where(p => p.Id == projectID).SelectMany(r => r.ProjectResource.Select(c => c.Resource)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            //var viewModel = new ProjectViewModel
+            //{
+            //    Teams = _context.Teams.ToList()
+            //};
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Project project)
+        {
+            //_context.Projects.Add(project);
+            //_context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TeamResourceTool/ViewModels/ProjectResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeamResourceTool.Models;
+
+namespace TeamResourceTool.ViewModels
+{
+    public class ProjectDetails
+    {
+        public Project Project { get; set; }
+        public IEnumerable<Resource> Resources { get; set; }
+    }
+}
