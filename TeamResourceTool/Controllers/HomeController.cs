1771c4b25075d1fcaffb7a815374f95f9d7aab17
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2018-06-28 14:22:34 -0700

    load team data from db and show in view

--- TeamResourceTool/Controllers/HomeController.cs
+++ TeamResourceTool/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamResourceTool.Models;
 
 namespace TeamResourceTool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var teams = GetTeams();
+            return View(teams);
         }
 
         public ActionResult About()
@@ -25,6 +39,18 @@ namespace TeamResourceTool.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        // GET: Teams
+        private IEnumerable<Team> GetTeams()
+        {
+            return _context.Team.ToList();
+        }
+
+        // GET: Team/ProjectList/id
+        private IEnumerable<Project> GetProjects(int id)
+        {
+            return _context.Project.Where(t => t.TeamId == id).ToList();
         }
     }
 }
