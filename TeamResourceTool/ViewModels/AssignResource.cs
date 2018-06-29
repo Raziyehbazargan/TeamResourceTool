6df8790acddf59ba275d1faa64874f7006a295af
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2018-06-29 16:21:43 -0700

    working ona ssignresource view

---
+++ TeamResourceTool/ViewModels/AssignResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeamResourceTool.Models;
+
+namespace TeamResourceTool.ViewModels
+{
+    public class AssignResource
+    {
+        public IEnumerable<Resource> Resources { get; set; }
+        public ProjectResource ProjectResource { get; set; }
+    }
+}
