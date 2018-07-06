5cc8b172fa19c68d99b5aa8930ee501e9863774c
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2018-07-06 11:59:24 -0700

    Add Data Transfer Object (DTO)

--- TeamResourceTool/App_Start/WebApiConfig.cs
+++ TeamResourceTool/App_Start/WebApiConfig.cs
@@ -9,6 +9,10 @@ namespace TeamResourceTool
     {
         public static void Register(HttpConfiguration config)
         {
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
---
+++ TeamResourceTool/Dtos/ProjectDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamResourceTool.Dtos
+{
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Project Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Display(Name = "Build Start Date")]
+        public DateTime? BuildStart { get; set; }
+
+        public DateTime? GoLive { get; set; }
+
+        public DateTime? EventStartDate { get; set; }
+
+        public DateTime? EventEndDate { get; set; }
+
+        public string Client { get; set; }
+
+        public string Priority { get; set; }
+
+        public string BuildSpecPath { get; set; }
+
+        [Required]
+        public bool NeedOnsiteSupport { get; set; }
+
+        public string EventLocation { get; set; }
+
+        public string Description { get; set; }
+
+        //public IEnumerable<Resource> Resources { get; set; }
+
+        //Team to projects : one to many Relationship mapping
+        [Display(Name = "Team")]
+        public byte TeamId { get; set; }
+       // public virtual Team Team { get; set; }
+
+
+        //Project to Resource : many to many Relationship mapping
+       // public virtual ICollection<ProjectResource> ProjectResource { get; set; }
+    }
+}
